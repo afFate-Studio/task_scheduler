e8810e426282d1ac422117895f6b67df5b4b3a5f
Fate_Studios <terrestrialabyss@gmail.com>
2023-06-02 17:04:27 -0400

    Added new file with very basic layout for the save and load functionality

---
+++ load.py
@@ -0,0 +1,8 @@
+# class used to save and write to files
+# in order to save tasks as an actual list that a user can refer to
+# as well as email the list as a pdf or txt
+class Save:
+    pass
+
+class Load:
+    pass
