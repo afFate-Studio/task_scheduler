69d6206ca493a812c83f9acfd1b486918c98bb0c
afFate-Studio <56927570+afFate-Studio@users.noreply.github.com>
2023-06-08 15:20:00 +0000

    Added more comments to further understanding of functionality in main.py

--- main.py
+++ main.py
@@ -11,9 +11,15 @@ from set_task import *                  # module used to set the task informatio
 def main():
     tasks = [] # empty list for task objects to be appended to
 
-    # Prompt user for a file path
+    """
+        Asks user if they have a list already made that they would like to add to
+        Then checks the response to make sure it is a valid response
+        Prompts user for the full file path depending on the users answer
+        File_Handling checks the file path, if there is no file found it will prompt the user
+        to choose either to create a new file or bypass the upload
+    """
     response = pyip.inputStr("Do you have a list file you would like to add to? ( Y | N ) : ")
-    response = check_response(response)
+    response = check_response(response) # checks for valid response
     if response in ('Y','y','Yes','yes'):
         file_name = pyip.inputFilepath("Please enter the full file path : ")
         f = File_Handling(file_name)    # tasks file path and sets the variable
@@ -21,6 +27,7 @@ def main():
     else:
         file_name = ""  # resets file_name to empty string
 
+    # loops until user decides they have no more tasks to add
     while True:
 
         task_info = get_task_info() # gets all tasks info (name, completion status, reminder, priority, comment) then stores it into a variable
@@ -29,17 +36,24 @@ def main():
 
         # TODO sort tasks based on priority
 
-        # Ask user if they would like to add another task, if not ask if they would like to email the list
+        """
+            Asks user if they would like to continue adding more tasks,
+            if yes the loop continues
+            otherwise if the file wasn't provided by the user the user will be asked
+            if they would like to save their file and be prompted to give a file name
+            if they said yes.
+            If file is provided the user will be asked if they would like to save to
+            the provided file.
+        """
         response = pyip.inputStr("\nWould you like to enter another task ( Y | N ): ")
         response = check_response(response) # check for valid response
         
-        # checks user response to if they wish to continue, if yes loop will continue otherwise user will be asked if they would like to email their list
-        # and loop will end
         if response in ('Y', 'y', 'Yes', 'yes'):
             continue
         else:
             if file_name == "":
                 response = pyip.inputStr("\nWould you like to save your task list to a file ( Y | N ): ")
+                response = check_response(response)
                 if response in ('Y','y','Yes','yes'):
                         file_name = pyip.inputFilepath("Please enter a file name you would like: ")
                         f = File_Handling(file_name, tasks)
@@ -47,18 +61,25 @@ def main():
                 else:
                     print("You chose to not save your task list to a file.")
             else:
-                f = File_Handling(file_name, tasks)
-                f.saving()
+                response = pyip.inputStr("\nWould you like to save the new tasks to the file you provided? ( Y | N ): ")
+                response = check_response(response)
+                if response in ('Y','y','Yes','yes'):
+                    f = File_Handling(file_name, tasks)
+                    f.saving()
+                else:
+                    print("You chose not to save your tasks.")
 
+            """
+                User is asked if they would like to email their task list
+                if yes, email will be sent.
+            """
             response = pyip.inputStr("\nWould you like to email your task list ( Y | N ): ") # prompts user asking if they would like to email their task list
             response = check_response(response)    # checks user response
             if response in ('Y', 'y', 'Yes', 'yes'):
                 Emailer(task) # checks response and sends on doesn't send email based on the response
             else:
                 print("Task list will not be emailed")   # if the user picks No, tell the user the list will not be emailed
-                    
-
-        
+                
             break # break main loop
 
 # TODO allow user to send task list to email or export as txt
