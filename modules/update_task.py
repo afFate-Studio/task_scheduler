87cc4f942c73723389a39b33320a21927fd6ad6f
afFate-Studio <56927570+afFate-Studio@users.noreply.github.com>
2023-06-19 07:28:55 -0400

    Update update_task.py

--- modules/update_task.py
+++ modules/update_task.py
@@ -1,1 +1,6 @@
-# TODO
+# TODO 
+# print all of the task names
+# allow user to put one of the tasks based on its name
+# print all of the task's elements
+# allow user to change what they want in the task
+# update the selected task in the dictionary
